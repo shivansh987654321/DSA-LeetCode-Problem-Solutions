ddf177603fb2770a70714b47df97bf7d3a0b60bb
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-19 21:18:46 +0530

    Time: 5 ms (16.91%) | Memory: 43.4 MB (58.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,45 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack <Character> stack = new Stack<>();
+        
+//         for(int i = 0; i < s.length(); i++){
+//             if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{' ){
+//                 stack.push(s.charAt(i));
+//             }
+//             else if(stack.isEmpty()){
+//                 return false;
+//             }else if (s.charAt(i) == ')' && stack.peek() != '(' || s.charAt(i) == ']' && stack.peek() != '[' ||s.charAt(i) == '}' && stack.peek() != '{'){
+//                 return false;
+//             }else{
+//                 stack.pop();
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+import java.util.*;
+
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> stack = new Stack<>();
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{' ){
-                stack.push(s.charAt(i));
-            }
-            else if(stack.isEmpty()){
-                return false;
-            }else if (s.charAt(i) == ')' && stack.peek() != '(' || s.charAt(i) == ']' && stack.peek() != '[' ||s.charAt(i) == '}' && stack.peek() != '{'){
-                return false;
-            }else{
-                stack.pop();
+        Stack<Character>st=new Stack<>();
+
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            } 
+            else{
+                if(st.isEmpty()) return false;
+
+                char top=st.pop();
+
+                if((ch==')' && top!='(') ||
+                    (ch=='}' && top!='{') ||
+                    (ch==']' && top!='[')) {
+                    return false;
+                }
             }
         }
-        return stack.isEmpty();
+
+        return st.isEmpty();
     }
 }
