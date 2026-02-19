511a2a7098677531a4be98625e0195393eb2a847
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-20 01:22:51 +0530

    Time: 59 ms (78.66%) | Memory: 103.2 MB (93.03%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         Stack<Integer> stack = new Stack<>();
+//         int n = temperatures.length;
+//         int [] result = new int[n];
+//         for(int i = n - 1 ; i>=0 ; i--){
+//             while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
+//                 stack.pop();
+//             }
+//             if(!stack.isEmpty()){
+//                 result[i] = stack.peek() - i;
+//             }
+//             stack.push(i);
+//         }
+//         return result;
+//     }
+// }
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-        int n = temperatures.length;
-        int [] result = new int[n];
-        for(int i = n - 1 ; i>=0 ; i--){
-            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
-                stack.pop();
+        int[] result = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i=0; i<temperatures.length;i++){
+            while (!stack.isEmpty() && temperatures[i] > stack.peek()[0]) {
+                int[] tempAndIndex = stack.pop();
+                result[tempAndIndex[1]] = i - tempAndIndex[1];
             }
-            if(!stack.isEmpty()){
-                result[i] = stack.peek() - i;
-            }
-            stack.push(i);
+            stack.push(new int[]{temperatures[i], i});
         }
+
         return result;
     }
 }
