a981faf72cadd4a52d8fe501ee5d4ddaf2c91703
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-19 10:49:14 +0530

    Time: 7 ms (69.95%) | Memory: 44.1 MB (64.88%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,33 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-    // step -1 we will count the frquency of the characters in the String s1
-    // step -2 then in the string 2 we will find the contigious subarray that contain the same frequency of characters in String -1 
-        if(s1.length() > s2.length()){
+        if (s1.length() > s2.length()){
             return false;
         }
-        int [] farr = new int [26];
-        int [] window = new int [26];
+        int [] freq = new int [26];
+        int [] freq2 = new int [26];
 
-        for( char c : s1.toCharArray()){
-            farr[c - 'a']++;
+        for(char c : s1.toCharArray()){
+            freq[c - 'a']++;
         }
+
         int left = 0;
-        for(int right  = 0; right < s2.length() ; right++){
-            window[s2.charAt(right) - 'a']++;
-            if(right - left + 1 > s1.length()){
-                window[s2.charAt(left) - 'a']--;
+        for(int right = 0 ;right < s2.length() ; right++){
+            freq2[s2.charAt(right) - 'a']++;
+            if((right - left + 1) > s1.length()){
+                freq2[s2.charAt(left) - 'a']--;
                 left++;
             }
 
-            if((right - left + 1) == s1.length()){
-                if(matches(farr , window)){
-                    return true ;
+            if((right - left + 1 ) == s1.length()){
+                if(matches(freq , freq2)){
+                    return true;
                 }
             }
         }
-        return false;
+        return false ;
     }
-
-    private boolean matches(int[] a , int[] b){
-        for(int i = 0;i < 26; i++){
+    private boolean matches(int a[] , int b[]){
+        for(int i = 0; i< 26 ;i++){
             if(a[i] != b[i]){
                 return false;
             }
