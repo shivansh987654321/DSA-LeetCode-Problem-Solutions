e462dde140c2415cd58dec283cdce599aa5d5563
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-22 14:02:21 +0530

    Time: 24 ms (46.17%) | Memory: 48.3 MB (50.13%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> st = new Stack<>();
+        int n = s.length();
         int cn = 0;
         char po = '+';
-        int n = s.length();
-        for(int i = 0;i < n; i++){
+        for(int i = 0; i< n; i++){
             char ch = s.charAt(i);
-        
             if(Character.isDigit(ch)){
                 cn = cn * 10 + (ch - '0');
             }
             if(!Character.isDigit(ch) && ch != ' ' || i == n-1){
                 if(po == '+'){
                     st.push(cn);
-                }if(po == '-'){
+                }
+                if(po == '-'){
                     st.push(-cn);
-                }if(po == '*'){
-                    st.push(st.pop() * cn);
-                }if(po == '/'){
+                }
+                if(po == '*'){
+                    st.push(cn * st.pop());
+                }
+                if(po == '/'){
                     st.push(st.pop() / cn);
                 }
                 po = ch;
@@ -26,7 +28,7 @@ class Solution {
         }
         int result = 0;
         while(!st.isEmpty()){
-            result = result + st.pop();
+            result  = result + st.pop();
         }
         return result;
     }
