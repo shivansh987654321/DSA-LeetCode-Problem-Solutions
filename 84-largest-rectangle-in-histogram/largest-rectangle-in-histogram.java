812864cee9f249ece2a896e38e514362ec870389
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-22 23:41:56 +0530

    Time: 70 ms (45.60%) | Memory: 78.5 MB (31.47%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,86 +1,23 @@
+import java.util.*;
+
 class Solution {
-    public int largestRectangleArea(int[] a) {
-        List<Integer> right = nsr(a);
-		List<Integer> left = nsl(a);
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
 
-		int maxArea = 0;
-		for (int i = 0; i < a.length; i++) {
-			maxArea = Math.max(maxArea, (right.get(i) - left.get(i) - 1) * a[i]);
-		}
-		return maxArea;
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
     }
-    
-    private List<Integer> nsl(int a[]) {
-		List<Integer> res = new ArrayList<>();
-		Stack<Pair> s = new Stack<>();
-		for (int i = 0; i < a.length; i++) {
-			if (s.size() == 0) {
-				res.add(-1);
-			} else if (s.size() > 0 && s.peek().getNge() < a[i]) {
-				res.add(s.peek().getIndex());
-			} else if (s.size() > 0 && s.peek().getNge() >= a[i]) {
-				while (s.size() > 0 && s.peek().getNge() >= a[i]) {
-					s.pop();
-				}
-				if (s.size() == 0) {
-					res.add(-1);
-				} else {
-					res.add(s.peek().getIndex());
-				}
-			}
-			s.add(new Pair(a[i], i));
-		}
-		return res;
-	}
-
-	// nsr: it has right so start with right and it has smaller so user <
-	private List<Integer> nsr(int a[]) {
-		List<Integer> res = new ArrayList<>();
-		Stack<Pair> s = new Stack<>();
-		for (int i = a.length - 1; i >= 0; i--) {
-			if (s.size() == 0) {
-				res.add(a.length );
-			} else if (s.size() > 0 && s.peek().getNge() < a[i]) {
-				res.add(s.peek().getIndex());
-			} else if (s.size() > 0 && s.peek().getNge() >= a[i]) {
-				while (s.size() > 0 && s.peek().getNge() >= a[i]) {
-					s.pop();
-				}
-				if (s.size() == 0) {
-					res.add(a.length );
-				} else {
-					res.add(s.peek().getIndex());
-				}
-
-			}
-			s.add(new Pair(a[i], i));
-		}
-		Collections.reverse(res);
-		return res;
-	}
-}
-class Pair {
-	int nge;
-	int index;
-
-	Pair(int nge, int index) {
-		this.nge = nge;
-		this.index = index;
-	}
-
-	public int getNge() {
-		return nge;
-	}
-
-	public void setNge(int nge) {
-		this.nge = nge;
-	}
-
-	public int getIndex() {
-		return index;
-	}
-
-	public void setIndex(int index) {
-		this.index = index;
-	}
 }
