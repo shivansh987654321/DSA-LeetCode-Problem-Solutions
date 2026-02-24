4d92157267f11acdc991446752e6bbc244462950
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-02-24 20:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (11.03%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,58 +1,29 @@
-// step -1 Check the list is empty or not  && check if right and left are at same index 
-// step -2 Make a dummy node and traverse the list upto just before the left 
-// step -3 Reverse the LinkedList upto right from left 
-// step -4 connect the node that was just before the left to the head of the reversed linked list 
-// step -5 connect the tail of the reversed linked list to the node just after the right
-
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null ||right == left ){
+        if(head == null || left == right){
             return head;
         }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
-        ListNode prevhead = dummy;
+        ListNode PrevHead = dummy;
         ListNode curr = head;
 
-        for(int i = 0; i< left - 1; i++){
-            prevhead = prevhead.next;
+        for(int i = 0 ; i < left - 1 ; i++){
+            PrevHead = PrevHead.next;
             curr = curr.next;
         }
-
-        ListNode sublisthead = curr;
+        ListNode SubListHead = curr;
         ListNode prev = null;
-        for(int i = 0; i< right - left + 1; i++){
+        for(int i = 0;i < right - left + 1; i++ ){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
 
-        prevhead.next = prev;
-        sublisthead.next = curr;
+        SubListHead.next = curr;
+        PrevHead.next = prev;
         return dummy.next;
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
