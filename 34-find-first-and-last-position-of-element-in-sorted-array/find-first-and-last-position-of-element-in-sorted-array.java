1eb1681d07e64ae4ed6d55bcde5938be194cae3f
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-03-10 21:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (68.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,44 @@
 class Solution {
-
     public int[] searchRange(int[] nums, int target) {
+        int left  = Left(nums , target);
+        int right = Right(nums , target);
 
-        int left = findLeftBound(nums, target);
-        int right = findRightBound(nums, target);
-
-        return new int[] { left, right };
+        return new int[] {left , right};
     }
-
-    private int findLeftBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                index = mid;
-                high = mid - 1;   // search left side
-            } 
-            else if (nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+    private int Left(int[] nums , int target){
+            int index = -1;
+            int low = 0;
+            int high = nums.length - 1;
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                if(nums[mid] == target){
+                    index = mid;
+                    high = mid - 1;
+                }
+                else if (nums[mid] < target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+            return index;
         }
-
-        return index;
-    }
-
-    private int findRightBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                index = mid;
-                low = mid + 1;   // search right side
-            } 
-            else if (nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+        private int Right(int[] nums , int target){
+            int index = -1;
+            int low = 0;
+            int high = nums.length - 1;
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                if(nums[mid] == target){
+                    index = mid;
+                    low = mid + 1;
+                }
+                else if (nums[mid] < target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+            return index;
         }
-
-        return index;
-    }
 }
