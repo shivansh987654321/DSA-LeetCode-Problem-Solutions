8f99d188f688fb1761eefb4df83e670a9be6ccff
shivansh987654321 <shivanshagrawal334@gmail.com>
2026-03-23 15:54:05 +0530

    Time: 16 ms (92.28%) | Memory: 82.3 MB (93.16%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,48 +1,47 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        
         if ((long) m*k>bloomDay.length){
             return -1;
         }
-        int low = bloomDay[0]; int high = bloomDay[0];
-        for (int i=0; i<bloomDay.length; i++){
-            if (bloomDay[i]<low){
+
+        int low = bloomDay[0];
+        int high = bloomDay[0];
+        for(int i = 0; i < bloomDay.length; i++){
+            if(bloomDay[i] < low){
                 low = bloomDay[i];
-            } else if (bloomDay[i]>high){
+            }else if(bloomDay[i] > high){
                 high = bloomDay[i];
             }
         }
-
-        int answer = -1;
-        
-        while (low<=high){
-            int mid = low + (high-low)/2;
-            if (isTrue(bloomDay, m, k, mid)){
-                answer = mid;
-                high = mid-1;
-            } else {
-                low = mid+1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isTrue(bloomDay , m , k , mid)){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        return answer;
+        return ans;
     }
-
-    private boolean isTrue(int[] bloomDay, int m, int k, int mid){
-        int counter = 0; int bouquet = 0;
-        for (int i=0; i<bloomDay.length; i++){
-            if (bloomDay[i]<=mid){
+    private boolean isTrue(int [] bloomDay , int m , int k , int mid){
+        int counter = 0;
+        int bouquet = 0;
+        for(int i = 0; i < bloomDay.length; i++){
+            if(bloomDay[i] <= mid){
                 counter++;
-                if (counter==k){
+                if(counter == k){
                     bouquet++;
-                    counter=0;
-                }
-            } else {
+                    counter = 0;
+                } 
+            }else{
                 counter = 0;
             }
         }
-        if (bouquet<m){
+        if(bouquet < m){
             return false;
-        } else if (bouquet==m) {
+        }else if (bouquet == m){
             return true;
         }
         return true;
