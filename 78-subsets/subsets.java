33566c73b711e5ef0caee2ca72fb0b5cc3293a72
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-04-02 00:32:33 +0530

    Time: 1 ms (88.28%) | Memory: 44.2 MB (48.92%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,18 +2,18 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> arr = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        subset(arr , ans , nums , 0);
+        rec(arr, ans, nums, 0);
         return arr;
-        
     }
-    private void subset(List<List<Integer>> arr , List<Integer> ans, int[] nums, int i){
+    private void rec(List<List<Integer>> arr , List<Integer> ans, int[] nums, int i){
         if(i == nums.length){
-            arr.add(new ArrayList<Integer> (ans));
+            arr.add(new ArrayList<>(ans));
             return;
         }
-        subset(arr , ans , nums , i + 1);
+        rec(arr, ans, nums, i + 1);
         ans.add(nums[i]);
-        subset(arr , ans , nums , i + 1);
+        rec(arr, ans, nums, i + 1);
         ans.remove(ans.size() - 1);
+
     }
 }
