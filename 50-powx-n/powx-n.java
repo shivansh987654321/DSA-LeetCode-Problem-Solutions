954fb5b164cdeb382ec3c58a95aa31e9a03f77d2
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-05-03 11:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (21.86%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        long N = n;
+
+        if(N == 0){
+            return 1;
+        }
+        if(N < 0){
+            x = 1 / x;
+            N = - N;
+        }
+
+        while(N > 0){
+            if(N % 2 == 1){
+                ans = ans * x;
+                N = N - 1;
+            }
+            x = x * x;
+            N = N / 2;
+        }
+        return ans;
+    }
+}
