df6ccbf3a317735f8f786c487c2ab80659bde494
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-05-03 13:02:52 +0530

    Time: 1 ms (10.11%) | Memory: 48.4 MB (10.84%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,22 +2,27 @@ class Solution {
     public double myPow(double x, int n) {
         double ans = 1;
         long N = n;
-
-        if(N == 0){
-            return 1;
-        }
+        
+        // if n is negative then reciprocal it with 1
         if(N < 0){
             x = 1 / x;
             N = - N;
         }
+        
+        // if n is zero then the ans must be 1 
+        if(N == 0){
+            return 1;
+        }
 
+        //for even x = x * x and for odd ans = ans * x
         while(N > 0){
-            if(N % 2 == 1){
+            if(N % 2 == 1){    // for odd
                 ans = ans * x;
-                
             }
-            x = x * x;
-            N = N / 2;
+
+            x = x * x;  // for even
+            N = N / 2;  // for even
+
         }
         return ans;
     }
