65d20f2cfe182f6c16b88c37dd05aae7a2f1ed1b
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-05-03 13:13:08 +0530

    Time: 6 ms (49.29%) | Memory: 55.7 MB (46.84%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,7 @@
-import java.util.*;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            } else {
-                map.put(num, 1);
-            }
-
-            // check majority condition
-            if (map.get(num) > nums.length / 2) {
-                return num;
-            }
-        }
-
-        return -1;
+        int n = nums.length;
+        Arrays.sort(nums);
+        return nums[n / 2];
     }
 }
