cc70b5d6972c9e5e91b50a8f5c0e1e0d523a1f5e
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-05-12 01:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (95.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,62 +1,39 @@
-import java.util.*;
-
 class Solution {
-    // Function to calculate trapped rainwater using the optimal two-pointer approach
     public int trap(int[] height) {
         int n = height.length;
-        
-        // Initialize two pointers at both ends of the array
+
         int left = 0;
         int right = n - 1;
-        
-        // Variables to track the maximum height to the left and right
+
         int maxLeft = 0;
         int maxRight = 0;
-        
-        // Variable to store total trapped water
+
         int totalWater = 0;
-        
-        // Iterate until left pointer meets right pointer
+
         while (left <= right) {
-            // If left bar is smaller or equal to right bar
+
             if (height[left] <= height[right]) {
-                // If current left bar is higher than maxLeft, update maxLeft
+
                 if (height[left] >= maxLeft) {
                     maxLeft = height[left];
                 } else {
-                    // Water trapped on left is difference between maxLeft and current height
                     totalWater += maxLeft - height[left];
                 }
-                left++; // Move left pointer to the right
+
+                left++;
+
             } else {
-                // If current right bar is higher than maxRight, update maxRight
+
                 if (height[right] >= maxRight) {
                     maxRight = height[right];
                 } else {
-                    // Water trapped on right is difference between maxRight and current height
                     totalWater += maxRight - height[right];
                 }
-                right--; // Move right pointer to the left
+
+                right--;
             }
         }
-        
-        // Return total trapped water
-        return totalWater;
-    }
-}
 
-public class Main {
-    public static void main(String[] args) {
-        // Input elevation map
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        
-        // Create Solution object
-        Solution sol = new Solution();
-        
-        // Calculate trapped water
-        int result = sol.trap(height);
-        
-        // Print the result
-        System.out.println("Trapped Rainwater: " + result);
+        return totalWater;
     }
 }
