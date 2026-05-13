3bc6efb77d4f95e276b579b9464235c262442d88
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-05-14 01:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (10.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,27 +1,38 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
+import java.util.*;
 
-            if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target <= nums[mid]){
-                    right = mid - 1;
-                }else{
-                    left = mid + 1;
+class Solution {
+    // Function to search for the target element in a rotated sorted array
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        // Applying binary search algorithm 
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // Check if mid points to the target
+            if (nums[mid] == target) return mid;
+
+            // Check if the left part is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    // Target exists in the left sorted part
+                    high = mid - 1;
+                } else {
+                    // Target does not exist in the left sorted part
+                    low = mid + 1;
                 }
-            }else{
-                if(target <= nums[right] && nums[mid] <= target){
-                    left = mid + 1;
-                }else{
-                    right = mid - 1;
+            } else {
+                // Check if the right part is sorted
+                if (nums[mid] <= target && target <= nums[high]) {
+                    // Target exists in the right sorted part
+                    low = mid + 1;
+                } else {
+                    // Target does not exist in the right sorted part
+                    high = mid - 1;
                 }
             }
         }
+        // If target is not found
         return -1;
     }
 }
