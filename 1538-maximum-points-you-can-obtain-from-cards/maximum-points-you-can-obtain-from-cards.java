e19dfab21550b6dfd2f6dd169d35a629e7bf9232
shivansh agrawal <shivanshagrawal334@gmail.com>
2026-08-03 17:04:48 +0530

    Time: 2 ms (99.70%) | Memory: 61.6 MB (87.56%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,57 +1,20 @@
-// class Solution {
-//     public int maxScore(int[] cardPoints, int k) {
-//         int n = cardPoints.length;
-//         int max = 0;
-//         for(int i = 0; i <= k; i++){
-//             int sum = 0;
-//             int j = k - i;
-//             for(int left = 0; left < i; left++){
-//                 sum = sum + cardPoints[left];
-//             }
-//             for(int right = n - j; right < n; right++){
-//                 sum = sum + cardPoints[right];
-//             }
-//             max = Math.max(max, sum);
-//         }
-//         return max;
-//     }
-// }
-
 class Solution {
-    /* Function to calculate the maximum
-    score after picking k cards */
     public int maxScore(int[] cardPoints, int k) {
-        int lSum = 0, rSum = 0, maxSum = 0;
-
-        // Calculate the initial sum of the first k cards
-        for (int i = 0; i < k; i++) {
-            lSum += cardPoints[i];
-            
-            /* Initialize maxSum with the
-            sum of the first k cards */
-            maxSum = lSum;
+        int n = cardPoints.length;
+        int max_sum = 0;
+        int left_sum = 0;
+        int right_sum = 0;
+        for(int i = 0; i < k; i++){
+            left_sum = left_sum + cardPoints[i];
         }
-
-        // Initialize rightIndex to iterate array from last
-        int rightIndex = cardPoints.length - 1;
-        
-        for (int i = k - 1; i >= 0; i--) {
-            
-            // Remove the score of the ith card from left sum
-            lSum -= cardPoints[i];   
-            
-            /* Add the score of the card
-            from the right to the right sum */
-            rSum += cardPoints[rightIndex];  
-            
-            // Move to the next card from the right
-            rightIndex--; 
-
-            // Update maxSum with the maximum sum found so far
-            maxSum = Math.max(maxSum, lSum + rSum);
+        max_sum = left_sum;
+        int right_index = n - 1;
+        for(int i = k - 1; i >= 0; i--){
+            left_sum = left_sum - cardPoints[i];
+            right_sum = right_sum + cardPoints[right_index];
+            right_index--;
+            max_sum = Math.max(max_sum , left_sum + right_sum);
         }
-
-        // Return the maximum score found
-        return maxSum; 
+        return max_sum;
     }
 }
